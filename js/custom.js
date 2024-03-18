a342b0e3f56b01a8d99ed46cec24d6b5e53225ab
Antoine Ramstein <antoine.ramstein@epita.fr>
2024-03-18 02:23:57 +0100

    j'ai tout refait parce que pourquoi pas

---
+++ js/custom.js
@@ -0,0 +1,137 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+function flipCard() {
+    const card = document.querySelector('col-lg-3');
+    const card2 = document.querySelector('col-sm-6');
+    card.style.transform = card.style.transform === 'rotateY(180deg)' ? 'rotateY(0deg)' : 'rotateY(180deg)';
+    card2.style.transform = card2.style.transform === 'rotateY(180deg)' ? 'rotateY(0deg)' : 'rotateY(180deg)';
+  }
+// client section owl carousel
+$(".client_owl-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    dots: false,
+    nav: true,
+    navText: [],
+    autoplay: true,
+    autoplayHoverPause: true,
+    navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 2
+        }
+    }
+});
+
+
+
+/** google_map js **/
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(40.712775, -74.005973),
+        zoom: 18,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
+document.querySelectorAll('.button').forEach(button => {
+
+    let duration = 3000,
+        svg = button.querySelector('svg'),
+        svgPath = new Proxy({
+            y: null,
+            smoothing: null
+        }, {
+            set(target, key, value) {
+                target[key] = value;
+                if(target.y !== null && target.smoothing !== null) {
+                    svg.innerHTML = getPath(target.y, target.smoothing, null);
+                }
+                return true;
+            },
+            get(target, key) {
+                return target[key];
+            }
+        });
+
+    button.style.setProperty('--duration', duration);
+
+    svgPath.y = 20;
+    svgPath.smoothing = 0;
+
+    button.addEventListener('click', e => {
+        
+        e.preventDefault();
+
+        if(!button.classList.contains('loading')) {
+
+            button.classList.add('loading');
+
+            gsap.to(svgPath, {
+                smoothing: .3,
+                duration: duration * .065 / 1000
+            });
+
+            gsap.to(svgPath, {
+                y: 12,
+                duration: duration * .265 / 1000,
+                delay: duration * .065 / 1000,
+                ease: Elastic.easeOut.config(1.12, .4)
+            });
+
+            setTimeout(() => {
+                svg.innerHTML = getPath(0, 0, [
+                    [3, 14],
+                    [8, 19],
+                    [21, 6]
+                ]);
+            }, duration / 2);
+
+        }
+
+    });
+
+});
+
+function getPoint(point, i, a, smoothing) {
+    let cp = (current, previous, next, reverse) => {
+            let p = previous || current,
+                n = next || current,
+                o = {
+                    length: Math.sqrt(Math.pow(n[0] - p[0], 2) + Math.pow(n[1] - p[1], 2)),
+                    angle: Math.atan2(n[1] - p[1], n[0] - p[0])
+                },
+                angle = o.angle + (reverse ? Math.PI : 0),
+                length = o.length * smoothing;
+            return [current[0] + Math.cos(angle) * length, current[1] + Math.sin(angle) * length];
+        },
+        cps = cp(a[i - 1], a[i - 2], point, false),
+        cpe = cp(point, a[i - 1], a[i + 1], true);
+    return `C ${cps[0]},${cps[1]} ${cpe[0]},${cpe[1]} ${point[0]},${point[1]}`;
+}
+
+function getPath(update, smoothing, pointsNew) {
+    let points = pointsNew ? pointsNew : [
+            [4, 12],
+            [12, update],
+            [20, 12]
+        ],
+        d = points.reduce((acc, point, i, a) => i === 0 ? `M ${point[0]},${point[1]}` : `${acc} ${getPoint(point, i, a, smoothing)}`, '');
+    return `<path d="${d}" />`;
+}
